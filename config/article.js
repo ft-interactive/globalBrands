2f2d0ba5119e4630b227dba251e1149fc800b81c
digitalcampbell <christopher.campbell@ft.com>
2017-06-26 17:36:24 +0100

    added uuid

--- config/article.js
+++ config/article.js
@@ -1,11 +1,11 @@
 export default () => ({ // eslint-disable-line
 
   // link file UUID
-  id: 'to come',
+  id: '4b1a73d4-5a5f-11e7-b553-e2df1b0c3220',
 
   // canonical URL of the published page
   // https://ig.ft.com/brexit-treaty-database get filled in by the ./configure script
-  url: 'https://ig.ft.com/to come',
+  url: 'https://ig.ft.com/top-100-global-brands',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
