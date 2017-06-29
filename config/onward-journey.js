0e3c78cdf6052168f2dc0f28d28be33464b9cf43
digitalcampbell <christopher.campbell@ft.com>
2017-06-29 15:10:28 +0100

    Changed onward journey to commence

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,5 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 2, list: 'thing/ODg2MTNjYjQtNGYzZi00MTU0LTg4MDYtMWE4MTZjMDkyM2M5-U3BlY2lhbFJlcG9ydHM=' },
+    { rows: 2, list: 'thing/ZDZiYjYyMDItY2E5MS00MzA3LWI3OWMtZWFiNDNhMmRiYmQz-VG9waWNz' },
   ],
 });
